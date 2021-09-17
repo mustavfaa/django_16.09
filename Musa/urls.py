b88ebe6f87d2facc51fee86dd18eb4cadaba0e14
mustavfaa <72229762+mustavfaa@users.noreply.github.com>
2021-09-17 12:17:15 +0600

    Update urls.py

--- Musa/urls.py
+++ Musa/urls.py
@@ -8,12 +8,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('grappelli/', include('grappelli.urls')),
-    path('admin/', admin.site.urls),
-    path('api-auth/',include("rest_framework.urls")),
-    path('ckeditor/',include("ckeditor_uploader.urls")),
+    	path('admin/', admin.site.urls),
+    	path('api-auth/',include("rest_framework.urls")),
+    	path('ckeditor/',include("ckeditor_uploader.urls")),
    	path('api/v1/',include("MDShop.urls")),
-   	
-   
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
